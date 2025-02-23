c023ad53d7e557e28835695cdfff5c48a3031fb2
Paul Ding <paul.ding@student.tdsb.on.ca>
2025-02-22 20:45:53 -0500

    Added first trending displaycase. working on rotating feature. tired so theres unfuntional code in there.

--- brains.js
+++ brains.js
@@ -2,3 +2,19 @@ window.addEventListener('scroll', function() {
     const img = document.getElementById("displaybg");
     img.style.top = `calc(${window.scrollY}px - 100vh)`;
   });
+
+  //var trendingcur = ""; to be implemented later with description boxes with spotlight effect
+  var curposition = 0;
+  function trendingscroll(a){ //true = right
+    var target = document.getElementById("trendingrotate");
+    var maxvalue = Number(target.dataset.value);
+    if(a){
+      if(maxvalue > curposition){
+        curposition++;
+      }
+    }else{
+      if(curposition > 0){
+        curposition--;
+      }
+    }
+  }
