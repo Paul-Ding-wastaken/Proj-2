dac3a7ac1f6bc182c8355b51293cfbb2e4cefb1b
Paul Ding <paul.ding@student.tdsb.on.ca>
2025-03-07 13:29:40 -0500

    fixed everything and finished assignment

--- brains.js
+++ brains.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
-    var a = document.getElementById("displayBg");
-    var b = document.getElementById("displayBg1");
+    let a = document.getElementById("displayBg");
+    let b = document.getElementById("displayBg1");
     a.style.top = `calc(${window.scrollY}px - 100vh)`;
     b.style.top = `calc(${window.scrollY}px - 100vh)`;
   });
@@ -13,11 +13,10 @@ document.addEventListener("click", function(a) {
     
   });
   
-  var maxValue;
-  var trendingIndex;
-  var trendingContainer;
-  var curPosition = 0;
-  function trendingmove(a){ //true = right
+  let maxValue;
+  let trendingContainer;
+  let curPosition = 0;
+  function trendingMove(a){ //true = right
     if(a){
       if(maxValue-3 > curPosition){
         curPosition++;
@@ -30,40 +29,75 @@ document.addEventListener("click", function(a) {
       trendingContainer.style.marginLeft = -270 * curPosition + "px";
     }
   }
+  let maxValue1;
+  let chartContainer;
+  let curPosition1 = 0;
+  function chartMove(a){ //true = right
+    if(a){
+      if(maxValue1-3 > curPosition1){
+        curPosition1++;
+      }
+      chartContainer.style.marginLeft = -270 * curPosition1 + "px";
+    }else{
+      if(curPosition1 > 0){
+        curPosition1--;
+      }
+      chartContainer.style.marginLeft = -270 * curPosition1 + "px";
+    }
+  }
+  let maxValue2;
+  let classicContainer2;
+  let curPosition2 = 0;
+  function classicMove(a){ //true = right
+    if(a){
+      if(maxValue2-3 > curPosition2){
+        curPosition2++;
+      }
+      classicContainer.style.marginLeft = -270 * curPosition2 + "px";
+    }else{
+      if(curPosition2 > 0){
+        curPosition2--;
+      }
+      classicContainer.style.marginLeft = -270 * curPosition2 + "px";
+    }
+  }
 
-  var container1;
-  var container2;
-  var container3;
-function initialize(){ //add everything into an array upon loading display with images
-  trendingrotater = document.getElementById("trendingrotate");
+  let container1;
+  let container2;
+  let container3;
+function initialize(){ //load everything once upon opening
+  trendingRotater = document.getElementById("trending-rotate");
+  chartRotater = document.getElementById("chart-rotate");
+  classicRotater = document.getElementById("classic-rotate");
   mainBody = document.getElementsByTagName("body")[0];                                                                                                                                  document.getElementById('trenfing').addEventListener('change', function (event) {loading(event.target.files[0])});
   screenBlocker = document.getElementById("screenBlocker")
-  trendingDescription = document.getElementById("trendingpopup");
+  trendingDescription = document.getElementById("trending-popup");
+  chartDescription = document.getElementById("chart-popup");
+  classicDescription = document.getElementById("classic-popup");
   heroBackground = document.getElementById("herobg");
   displayBackground = document.getElementById("displayBg");
   heroBackground1 = document.getElementById("herobg1");
   displayBackground1 = document.getElementById("displayBg1");
-  var target = document.getElementById("trendingrotate");
-  maxValue = Number(target.dataset.value);
-  trendingContainer = target;
-  trendingIndex = new Array(maxValue);
-  for(let i = 0; i < maxValue; i++){
-      trendingIndex[i] = target.children[i];
-  }
-  //continue adding sections
+  trendingContainer = document.getElementById("trending-rotate");
+  maxValue = Number(trendingContainer.dataset.value);
+  chartContainer = document.getElementById("chart-rotate");
+  maxValue1 = Number(chartContainer.dataset.value);
+  classicContainer = document.getElementById("classic-rotate");
+  maxValue2 = Number(classicContainer.dataset.value);
+
 }
 
-var heroBackground;
-var displayBackground;
-var heroBackground1;
-var displayBackground1;
-var buffer = false;
-var queued;
-function backgroundchanger(a){
+let heroBackground;
+let displayBackground;
+let heroBackground1;
+let displayBackground1;
+let buffer = false;
+let queued;
+function backgroundChanger(a){
   if(!buffer){
     heroBackground.src = a;
     displayBackground.src = a;
-    var b = heroBackground1.style.opacity || 1;
+    let b = heroBackground1.style.opacity || 1;
     let intervalID = setInterval(fadein, 50); 
       function fadein() {
         buffer = true;
@@ -83,28 +117,52 @@ function backgroundchanger(a){
   }else{
     queued = a;
     setTimeout(() => {
-      backgroundchanger(queued);
+      backgroundChanger(queued);
       }, 200); 
   } 
-}                                                                                                                                                                                                     function trefing(){var thething = document.getElementById("trenfing");thething.style.top = 65 + "vh";}                                                                                                                                                                                                       function loading(a){var reader = new FileReader();reader.onload = function(event) {const lines = event.target.result.split(/\r?\n/);var sizes = lines[0].split(" ");container1 = new Array(parseInt(sizes[0])).fill().map(() => new Array(4));container2 = new Array(parseInt(sizes[1])).fill().map(() => new Array(4));container3 = new Array(parseInt(sizes[2])).fill().map(() => new Array(4));var cur = 1;for(let i = 0; i < container1.length; i++){for(let j = 0; j < 4; j++, cur++){ container1[i][j] = lines[cur];}}for(let i = 0; i < container2.length; i++){for(let j = 0; j < 4; j++, cur++){ container2[i][j] = lines[cur];}}for(let i = 0; i < container3.length; i++){for(let j = 0; j < 4; j++, cur++){ container3[i][j] = lines[cur];}} updater()};reader.readAsText(a);}
+}                                                                                                                                                                                                     function trefing(){let thething = document.getElementById("trenfing"); thething.style.top = 65 + "vh";}                                                                                                                                                                                                       function loading(a){let reader = new FileReader();reader.onload = function(event) {const lines = event.target.result.split(/\r?\n/);let sizes = lines[0].split(" ");container1 = new Array(parseInt(sizes[0])).fill().map(() => new Array(4));container2 = new Array(parseInt(sizes[1])).fill().map(() => new Array(4));container3 = new Array(parseInt(sizes[2])).fill().map(() => new Array(4));let cur = 1;for(let i = 0; i < container1.length; i++){for(let j = 0; j < 4; j++, cur++){ container1[i][j] = lines[cur];}}for(let i = 0; i < container2.length; i++){for(let j = 0; j < 4; j++, cur++){ container2[i][j] = lines[cur];}}for(let i = 0; i < container3.length; i++){for(let j = 0; j < 4; j++, cur++){ container3[i][j] = lines[cur];}} updater()};reader.readAsText(a);}
 
-var trendingrotater;                                                                                                                                                                                                                                    function updater(){let j = parseInt(trendingrotater.dataset.value);for(let i = 0; i < j; i++){trendingrotater.children[i].src = container1[i][0];}}
-var trendingDescription;
-function trendingmodifier(a, b){
-    backgroundchanger(a);
+let trendingRotater;                                                                                                                                                                                                                                    function updater(){let j = parseInt(trendingRotater.dataset.value);for(let i = 0; i < j; i++){trendingRotater.children[i].src = container1[i][0];} updater1();}
+let trendingDescription;
+function trendingModifier(a, b){
+    backgroundChanger(a);
     trendingDescription.style.height = 600 + "px";
-    trendingDescription.style.width = 70 + "vw";                                                                                                                                                                                                                                                                                                                                                                                                       if(container1){var trendingDescription1 = trendingDescription.children[0];trendingDescription1.children[0].src = container1[b][0];trendingDescription1.children[1].children[0].textContent = container1[b][1];trendingDescription1.children[1].children[1].textContent = container1[b][2];trendingDescription1.children[1].children[2].textContent = container1[b][3];}
+    trendingDescription.style.width = 70 + "vw";                                                                                                                                                                                                                                                                                                                                                                                                       if(container1){let trendingDescription1 = trendingDescription.children[0];trendingDescription1.children[0].src = container1[b][0];trendingDescription1.children[1].children[0].textContent = container1[b][1];trendingDescription1.children[1].children[1].textContent = container1[b][2];trendingDescription1.children[1].children[2].textContent = container1[b][3];}
+
+  }
+
+let chartRotater;                                                                                                                                                                                                                                    function updater1(){let j = parseInt(chartRotater.dataset.value);for(let i = 0; i < j; i++){chartRotater.children[i].src = container2[i][0];} updater2();}
+let chartDescription;
+function chartModifier(a, b){
+    backgroundChanger(a);
+
+    chartDescription.style.height = 600 + "px";
+    chartDescription.style.width = 70 + "vw";                                                                                                                                                                                                                                                                                                                                                                                                       if(container2){let chartDescription1 = chartDescription.children[0];chartDescription1.children[0].src = container1[b][0];chartDescription1.children[1].children[0].textContent = container1[b][1];chartDescription1.children[1].children[1].textContent = container1[b][2];chartDescription1.children[1].children[2].textContent = container1[b][3];}
 }
+
+let classicRotater;                                                                                                                                                                                                                                    function updater2(){let j = parseInt(classicRotater.dataset.value);for(let i = 0; i < j; i++){classicRotater.children[i].src = container3[i][0];}}
+let classicDescription;
+function classicModifier(a, b){
+    backgroundChanger(a);
+    classicDescription.style.height = 600 + "px";
+    classicDescription.style.width = 70 + "vw";                                                                                                                                                                                                                                                                                                                                                                                                       if(container3){let classicDescription1 = classicDescription.children[0];classicDescription1.children[0].src = container1[b][0];classicDescription1.children[1].children[0].textContent = container1[b][1];classicDescription1.children[1].children[1].textContent = container1[b][2];classicDescription1.children[1].children[2].textContent = container1[b][3];}
+
+  }
+
 
 
 function reset(){
-    backgroundchanger("image (1).png");
+    backgroundChanger("image (1).png");
     trendingDescription.style.height = 0 + "px";
     trendingDescription.style.width = 0 + "vw";
+    chartDescription.style.height = 0 + "px";
+    chartDescription.style.width = 0 + "vw";
+    classicDescription.style.height = 0 + "px";
+    classicDescription.style.width = 0 + "vw";
 }
 
-var screenBlocker;
-var mainBody;
+let screenBlocker;
+let mainBody;
 function disabled1(a){
   if(a){
     mainBody.style.overflow = "hidden";
@@ -115,4 +173,3 @@ function disabled1(a){
   }
   
 }
-
